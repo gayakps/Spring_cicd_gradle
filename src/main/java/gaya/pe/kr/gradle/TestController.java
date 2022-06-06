410193d30dc79e2130da80548ff9190227a64b3c
seon.woo <gaya.kps@gmail.com>
2022-06-07 05:35:43 +0900

    Case 1

--- src/main/java/gaya/pe/kr/gradle/TestController.java
+++ src/main/java/gaya/pe/kr/gradle/TestController.java
@@ -1,18 +1,18 @@
-package gaya.pe.kr.cicd;
+package gaya.pe.kr.gradle;
 
-import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @GetMapping("/")
     public static String test(Model model) {
-        return "Testing won 22 06 07 5:33 Case 1";
+        return "Testing won 22 06 07 5:35 Case 1";
     }
 
 }
