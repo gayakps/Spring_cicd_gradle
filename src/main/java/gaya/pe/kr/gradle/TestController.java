c7c04f2716254973fba606e6b350e9904819490c
seon.woo <gaya.kps@gmail.com>
2022-06-13 17:58:32 +0900

    Test Case

--- src/main/java/gaya/pe/kr/gradle/TestController.java
+++ src/main/java/gaya/pe/kr/gradle/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/")
     public static String test(Model model) {
-        return "Testing won 22 06 13 17:49 Case 1";
+        return "Testing won 22 06 13 17:58 Case 1";
     }
 
 }
