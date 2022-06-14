017963eb2739efb12ce300e3214ab572306beaad
seon.woo <gaya.kps@gmail.com>
2022-06-14 15:09:32 +0900

    되어야 한다 이제! 마지막 설정 ( Health Checking 문제 없음 ) AllowTraffic 에서 미친듯이 안되어버림!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo "배포이후 시스템을 시작합니다" >> /home/ec2-user/deploy.log
+echo "> 배포이후 시스템을 시작합니다" >> /home/ec2-user/deploy.log
 BUILD_JAR=$(ls /home/ec2-user/build/libs/gradle-22.06.14.jar)     # jar가 위치하는 곳
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
+echo "> 시스템을 시작합니다 build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 echo "> build 파일 복사" >> /home/ec2-user/deploy.log
-DEPLOY_PATH=/home/ec2-user/
+DEPLOY_PATH=/home/ec2-user/deploy/
 cp $BUILD_JAR $DEPLOY_PATH
 
 CURRENT_PID=$(pgrep java)
-echo "> 현재 실행중인 애플리케이션 pid 확인 $CURRENT_PID" >> /home/ec2-user/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid = $CURRENT_PID" >> /home/ec2-user/deploy.log
 
 if [ -z $CURRENT_PID ]
 then
-  echo "현재 구동중인 어플리케이션이 없으므로 종료하지 않습니다" >> /home/ec2-user/deploy.log
+  echo "> 현재 구동중인 어플리케이션이 없으므로 종료하지 않습니다" >> /home/ec2-user/deploy.log
 else
-  echo "현재 구동중인 어플이 존재하므로 종료합니다" >> /home/ec2-user/deploy.log
+  echo "> 현재 구동중인 어플이 존재하므로 종료합니다" >> /home/ec2-user/deploy.log
   echo "> kill -15 $CURRENT_PID" >> /home/ec2-user/deploy.log
   kill -15 $CURRENT_PID
   sleep 10
--- src/main/java/gaya/pe/kr/gradle/GradleApplication.java
+++ src/main/java/gaya/pe/kr/gradle/GradleApplication.java
@@ -3,10 +3,13 @@ package gaya.pe.kr.gradle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class GradleApplication {
 
     public static void main(String[] args) {
+        System.out.println("[[[ Start System " + new Date().toLocaleString() + "]]]]");
         SpringApplication.run(GradleApplication.class, args);
     }
 
