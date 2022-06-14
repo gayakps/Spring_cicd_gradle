08ec397b4a81bfe7108ca5b15d83b385632706bc
seon.woo <gaya.kps@gmail.com>
2022-06-14 17:22:46 +0900

    되어야 한다 이제! 마지막 설정 ( Health Checking 문제 없음 ) AllowTraffic 에서 미친듯이 안되어버림!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ echo "> 22.06.14 15:20 배포이후 시스템을 시작합니다" >> $LOG_PATH
 CURRENT_PID=$(pgrep :8080)
 echo "> 현재 실행중인 애플리케이션 pid = $CURRENT_PID" >> $LOG_PATH
 
-if [ -z "$CURRENT_PID" ]
+if [ -z $CURRENT_PID ];
 then
   echo "> 현재 구동중인 어플리케이션이 없으므로 종료하지 않습니다" >> $LOG_PATH
 else
@@ -17,6 +17,9 @@ fi
 
 AFTER_PID=$(pgrep java)
 echo "현재 남은 PID : $AFTER_PID" >> $LOG_PATH
-echo "자바 서비스를 실행합니다 !!" >> $LOG_PATH
+echo "Starting Java Service" >> $LOG_PATH
 cat $LOG_PATH;
-nohup java -jar /home/ec2-user/build/libs/gradle-22.06.14.jar >> $LOG_PATH 2>/home/ec2-user/deploy_err.log &
+JAR_PATH=/home/ec2-user/build/libs/gradle-22.06.14.jar
+echo "JAR NAME : $JAR_PATH" >> $LOG_PATH
+chmod +x $JAR_PATH
+nohup java -jar $JAR_PATH >> $LOG_PATH 2>/home/ec2-user/deploy_err.log &
