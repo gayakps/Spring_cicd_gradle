178165cfb6583d20b4f8945248ca14e41c1cb5be
seon.woo <gaya.kps@gmail.com>
2022-06-14 13:09:28 +0900

    되어야 한다 이제! 마지막 설정 ( Health Checking 문제 없음 )

--- src/main/java/gaya/pe/kr/gradle/TestController.java
+++ src/main/java/gaya/pe/kr/gradle/TestController.java
@@ -7,12 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class TestController {
 
     @GetMapping("/")
     public static String test(Model model) {
-        return "Testing won 22 06 14 12:54 Case 1";
+        InetAddress ip = null;
+        try {
+            ip = InetAddress.getLocalHost();
+            return String.format("Testing won 22 06 14 12:54 Case 1 IP : %s HostName : %s", ip.getHostAddress(), ip.getHostName());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "IP 추출에 문제 발생 Testing won 22 06 14 12:54 Case 1";
     }
 
 }
